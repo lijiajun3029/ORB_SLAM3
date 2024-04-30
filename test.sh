5052888981849af852345b99dd8c8a87794b12e8
lijiajun1018 <lijiajun3029@gmail.com>
2024-04-30 18:21:48 +0800

    debug

---
+++ test.sh
@@ -0,0 +1,6 @@
+# EuROC数据集
+# 双目惯导
+./Examples/Stereo-Inertial/stereo_inertial_euroc ./Vocabulary/ORBvoc.txt ./Examples/Stereo-Inertial/EuRoC.yaml ./data/MH_01_easy ./Examples/Stereo-Inertial/EuRoC_TimeStamps/MH01.txt dataset-MH01_stereo_inertial
+
+# 另外也可以使用ros2，获得实时数据进行处理
+https://blog.csdn.net/liiiuzy/article/details/136272727
